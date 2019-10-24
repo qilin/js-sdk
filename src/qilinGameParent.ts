6b159f03ca2d21b82c6f656eb4d9158f36db0641
BeHuaMuH <sven1980@list.ru>
2019-10-24 16:01:46 +0300

    Add readme file

--- src/qilinGameParent.ts
+++ src/qilinGameParent.ts
@@ -1,3 +1,25 @@
-export default () => {
+import { PayFormCallback } from 'types';
+import { SHOW_PAYMENT_FORM, PAYMENT_FORM_CLOSED } from './constants';
 
+export default (payFormCallback: PayFormCallback) => {
+  const onFormClose = (frame: Window, status?: boolean) => {
+    const data = {
+      type: PAYMENT_FORM_CLOSED,
+      payload: { status },
+    };
+    frame.postMessage(data, '*');
+  };
+  window.addEventListener('message', (event: MessageEvent) => {
+    const frame = event.source as Window;
+    const { data = {} } = event;
+    const { type, payload = {} } = data;
+    if (type !== SHOW_PAYMENT_FORM) return;
+    const { gameUid, userId, itemId } = payload;
+    payFormCallback(gameUid, userId, itemId)
+      .then(status => onFormClose(frame, status))
+      .catch(err => {
+        console.error(err);
+        onFormClose(frame, false);
+      });
+  });
 };
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,3 @@
 export type Callback = () => void;
+
+export type PayFormCallback = (gameUid: string, userId: string, itemId: string) => Promise<any>;
