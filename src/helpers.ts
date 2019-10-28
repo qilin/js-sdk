a71a1e50ad6973c455aa9fc1083f4fc79f75a9fb
BeHuaMuH <sven1980@list.ru>
2019-10-28 07:40:27 +0300

    v1 is done

---
+++ src/helpers.ts
@@ -0,0 +1,5 @@
+export const getUrlParameter = (name: string) => {
+  const regex = new RegExp(`[\\?&]${name}=([^&#]*)`);
+  const results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-import qilineGameParent from './qilineGameParent';
-import qilineGameFrame from './qilineGameFrame';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import qilinGameParent from './qilinGameParent';
+import qilinGameFrame from './qilinGameFrame';
+import { PAYMENT_FORM_CLOSED } from './constants';
+
+if (!window) throw new Error('SDK only work on web browser');
+(window as any).qilinGameParent = qilinGameParent;
+(window as any).qilinGameFrame = qilinGameFrame;
+(window as any).PAYMENT_FORM_CLOSED = PAYMENT_FORM_CLOSED;
