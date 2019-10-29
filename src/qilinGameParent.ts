36c229fa5f186c8236e2698f747fd460f9948e2f
Behuamuh <sven1980@list.ru>
2019-10-29 05:56:20 +0300

    Start work on changes qilinParentGame

--- src/qilinGameParent.ts
+++ src/qilinGameParent.ts
@@ -1,10 +1,11 @@
-import { PayFormCallback } from 'types';
+// import { PayFormCallback } from 'types';
 import { SHOW_PAYMENT_FORM, PAYMENT_FORM_CLOSED } from './constants';
 
-export default (payFormCallback: PayFormCallback) => {
-  if (typeof payFormCallback !== 'function') throw new Error('PayFormCallback not provided!');
+export default (qilinProductUUID: string, apiURL: string) => {
+  if (!qilinProductUUID) throw new Error('Game UID is required, but not provided');
+  if (!apiURL) throw new Error('Api URL is required, but not provided');
 
-  const onFormClose = (frame: Window, status: boolean) => {
+  const onPayFormClose = (frame: Window, status: boolean) => {
     const data = {
       type: PAYMENT_FORM_CLOSED,
       payload: { status },
@@ -12,17 +13,44 @@ export default (payFormCallback: PayFormCallback) => {
     frame.postMessage(data, '*');
   };
 
-  window.addEventListener('message', (event: MessageEvent) => {
-    const frame = event.source as Window;
-    const { data = {} } = event;
-    const { type, payload = {} } = data;
-    if (type !== SHOW_PAYMENT_FORM) return;
-    const { qilinProductUUID, userId, itemId } = payload;
-    payFormCallback(qilinProductUUID, userId, itemId)
-      .then(status => onFormClose(frame, status))
-      .catch(err => {
+  const onAuthSuccess = () => {
+    window.addEventListener('message', (event: MessageEvent) => {
+      const frame = event.source as Window;
+      const { data = {} } = event;
+      const { type, payload = {} } = data;
+      if (type !== SHOW_PAYMENT_FORM) return;
+      const { qilinProductUUID, userId, itemId } = payload;
+
+      try {
+        // Логика БИЛЛИНГА возвращающая статус
+        const status = Math.random() < 0.5; // Пока берем случайный
+        onPayFormClose(frame, status);
+      } catch (err) {
         console.error(err);
-        onFormClose(frame, false);
+        onPayFormClose(frame, false);
+      }
+    });
+  };
+
+  const init = async (inputMeta: any) => {
+    try {
+      const response = await fetch(`${apiURL}/auth`, { // Вместо /auth - будет другой путь.
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ meta: inputMeta }),
       });
-  });
+
+      const json = await response.json();
+      onAuthSuccess();
+      return json.meta;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
+  return {
+    init,
+    // showPayForm,
+  };
 };
