4b99b6e4075b1a1e4e233c9f28ae8990d28431cc
Behuamuh <sven1980@list.ru>
2019-11-07 09:50:58 +0300

    Copy rgames script

---
+++ rambler-adapter/rgames.js
@@ -0,0 +1,89 @@
+! function (e) {
+  var t = {};
+
+  function n(o) {
+    if (t[o]) return t[o].exports;
+    var r = t[o] = {
+      i: o,
+      l: !1,
+      exports: {}
+    };
+    return e[o].call(r.exports, r, r.exports, n), r.l = !0, r.exports
+  }
+  n.m = e, n.c = t, n.d = function (e, t, o) {
+    n.o(e, t) || Object.defineProperty(e, t, {
+      enumerable: !0,
+      get: o
+    })
+  }, n.r = function (e) {
+    "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
+      value: "Module"
+    }), Object.defineProperty(e, "__esModule", {
+      value: !0
+    })
+  }, n.t = function (e, t) {
+    if (1 & t && (e = n(e)), 8 & t) return e;
+    if (4 & t && "object" == typeof e && e && e.__esModule) return e;
+    var o = Object.create(null);
+    if (n.r(o), Object.defineProperty(o, "default", {
+      enumerable: !0,
+      value: e
+    }), 2 & t && "string" != typeof e)
+      for (var r in e) n.d(o, r, function (t) {
+        return e[t]
+      }.bind(null, r));
+    return o
+  }, n.n = function (e) {
+    var t = e && e.__esModule ? function () {
+      return e.default
+    } : function () {
+      return e
+    };
+    return n.d(t, "a", t), t
+  }, n.o = function (e, t) {
+    return Object.prototype.hasOwnProperty.call(e, t)
+  }, n.p = "/", n(n.s = 0)
+}([function (e, t) {
+  window.rgames = function () {
+    var e = {};
+    return {
+      init: function () {
+        return window.addEventListener("message", function (e) {
+          var t = e.data;
+          window.rgames.emit(t.method, t.data)
+        }), Promise.resolve()
+      },
+      showOrderBox: function (e) {
+        window.top.postMessage({
+          method: "rgames-showOrderBox",
+          data: e
+        }, "*")
+      },
+      closeOrderBox: function (e) {
+        window.top.postMessage({
+          method: "rgames-closeOrderBox",
+          data: e
+        }, "*")
+      },
+      resizeWindow: function (e) {
+        window.top.postMessage({
+          method: "rgames-resizeWindow",
+          data: e
+        }, "*")
+      },
+      enableFullscreenMode: function () {
+        window.top.postMessage({
+          method: "rgames-fullscreenEnable"
+        }, "*")
+      },
+      addCallback: function (t, n) {
+        e[t] ? e[t].push(n) : e[t] = [n]
+      },
+      emit: function (t, n) {
+        e[t] && e[t].forEach(function (e) {
+          e(n)
+        })
+      }
+    }
+  }()
+}]);
