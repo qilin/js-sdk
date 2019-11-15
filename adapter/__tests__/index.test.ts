2f7c4984e58535e495bf031cb467e3c86c1e3dcb
Behuamuh <sven1980@list.ru>
2019-11-15 05:51:57 +0300

    Added index.ts test

---
+++ adapter/__tests__/index.test.ts
@@ -0,0 +1,7 @@
+import '../index';
+
+describe('Index file', () => {
+  it('QilinHub', () => {
+    expect((window as any).qilinHub).toBeDefined();
+  });
+})
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  globals: {
+    window: {
+      location: { href: '/' },
+      addEventListener: () => { },
+      parent: {},
+      fetch: () => { },
+    },
+  },
+  // automock: false,
+  // setupFiles: [
+  //   './setupJest.ts',
+  // ],
 };
