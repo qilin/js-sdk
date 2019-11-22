48a8f95622bf9e8f9fb0b09c923903498d865734
Behuamuh <sven1980@list.ru>
2019-11-22 06:30:36 +0300

    Removed margin, padding and border to iframe

--- src/openIframe.ts
+++ src/openIframe.ts
@@ -1,8 +1,12 @@
 export default (url: string) => {
   const iframe = document.createElement('iframe');
+
   iframe.src = url;
   iframe.style.width = '100%';
   iframe.style.height = '100%';
+  iframe.style.margin = '0';
+  iframe.style.padding = '0';
+  iframe.style.border = '0';
   iframe.width = '100%';
   iframe.height = '100%';
 
