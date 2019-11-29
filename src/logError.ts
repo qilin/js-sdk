c93aaee3161342ea901415dd8811385fa6bf7bf1
Behuamuh <sven1980@list.ru>
2019-11-29 09:21:09 +0300

    Changed Sentry initialization

--- src/logError.ts
+++ src/logError.ts
@@ -1,7 +1,15 @@
 import * as Sentry from '@sentry/browser';
 
 const isDevMod = process.env.NODE_ENV === 'development';
-Sentry.init({ dsn: process.env.SENTRY_DSN });
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.GlobalHandlers({
+      onerror: !isDevMod,
+      onunhandledrejection: !isDevMod,
+    }),
+  ],
+});
 
 export default (error: Error) => {
   if (!isDevMod) Sentry.captureException(error);
